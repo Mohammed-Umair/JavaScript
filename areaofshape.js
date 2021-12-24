b086d95f8e5a9111156fae4a3fb0670ec5a14c1a
mohammedumair123 <kasarumair786@gmail.com>
2021-12-24 14:32:52 +0530

    update

---
+++ areaofshape.js
@@ -0,0 +1,45 @@
+//area of Square
+// const areaOfSquare=(a)=>formulaofsquare=a*a
+
+// console.log(areaOfSquare(10))
+
+// area of rectangle
+// vanila method
+// function areaofRectangle(l,b) {
+//     formulaofRectangle = l * b
+//     return formulaofRectangle
+// }
+// console.log(areaofRectangle(5,4))
+
+//es6 mothod
+// const areaofRectangle=(l,b) => l*b
+// console.log(areaofRectangle(5,4))
+
+//area of trangle
+///vanila
+// function areaoftrangle(h,b){
+//     formulaoftrangle=h*b
+//     return formulaoftrangle
+// }
+// console.log(areaoftrangle(8,4))
+
+//es6
+// const areaoftrangle=(h,b)=> h*b
+// console.log(areaoftrangle(8,5))
+
+// area of circle
+
+
+function areaofcircle(r) {
+    pi=22/7
+    formulaofcircle=pi*r*r
+    return formulaofcircle
+
+}
+
+console.log(areaofcircle(5))
+
+// es6
+// const areaofcircle = (r) =>pi* r * r 
+// const pi=22/7
+// console.log(areaofcircle(5))
